929a482b73613e005dbda4c9c99d5ece0d3ed586
Dmitry Mozzherin <dmozzherin@gmail.com>
2023-12-19 22:15:33 +0000

    docs

--- pkg/ent/bhln/interface.go
+++ pkg/ent/bhln/interface.go
@@ -2,7 +2,9 @@ package bhln
 
 // BHLN takes data from BHLnames.
 type BHLN interface {
+	// ItemIds retuns back ids of BHL items that need to be embedded.
 	ItemIds(offset, limit int, taxa []string) ([]uint, error)
+
 	// Close cleans up database connections
 	Close()
 }
